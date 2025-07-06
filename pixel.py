a352e2d3d5c1c8a48cba73a7208c6444a8fbcdbb
Alexander M <aardvarkchase@gmail.com>
2025-07-06 17:41:18 -0400

    Fixed an error in getting the direction between two planets, made "force" methods purge used vectors

--- pixel.py
+++ pixel.py
@@ -10,6 +10,7 @@ class pixel:
         self.position = position  # (x,y) Tuple
         self.direction = pygame.Vector2(direction)  # (x,y) Vector2
         self.force = force  # float value
+        self.gForce = pygame.Vector2(0,0)
         self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
     def getPosition(self):
@@ -21,26 +22,27 @@ class pixel:
     def vector_set(self, direction, force):
             self.direction = direction
             self.force = force
+            self.gForce = self.direction * self.force
 
 
     def applyForce(self):
-        self.position = self.force * pygame.Vector2(self.direction)
+        #self.position = self.force * pygame.Vector2(self.direction)
+        self.position += self.gForce
+        self.gForce = pygame.Vector2(0,0)
+        self.direction = (0, 0)
+        self.force = 0
 
     def getDirection(self, p0, p1):
-        p0, p1 = pygame.Vector2(p0[0], p0[1]), pygame.Vector2(p1[0], p1[1])
-        distance = p0.distance_to(p1)
-        p2 = pygame.Vector2(p1[0] - p0[0], p1[1] - p0[0])
-        if(math.dist(p0, p1) == 0):
-            return pygame.Vector2(0, 0)
-        return p0.normalize()
+        p0_vec = pygame.Vector2(p0[0], p0[1])
+        p1_vec = pygame.Vector2(p1[0], p1[1])
+        direction = p1_vec - p0_vec
 
-    #def getDirection(self, p1): #Overloaded method which finds the direction relative to the referenced planet
-    #    p0, p1 = pygame.Vector2(self.position[0], self.position[1]), pygame.Vector2(p1[0], p1[1])
-    #    distance = p0.distance_to(p1)
-    #    p2 = pygame.Vector2(p1[0] - p0[0], p1[1] - p0[0])
-    #    print(p2.normalize())
-    #    return p0
+        if direction.length() == 0:
+            return pygame.Vector2(0, 0)  # No direction if points are the same
 
+        return direction.normalize()
+
+    @staticmethod
     def return_list_mass(pixel_array):
         temp_mass = 0
         for x in pixel_array:
@@ -48,8 +50,8 @@ class pixel:
         return temp_mass
 
     def gravity(self, g, obj0, obj1):
-        self.direction = self.getDirection(obj0.position, obj1.position)
-        print(f'obj0 position: {obj0.position}, obj1 position: {obj1.position}')
-        self.force += (g * obj0.mass * obj1.mass) / (math.dist(obj0.position, obj1.position) ** 2)
-
-        return (g * obj0.mass * obj1.mass) / (math.dist(obj0.position, obj1.position) ** 2)
+        self.direction = self.getDirection(obj0.position, obj1.position)    #Normalized Direction
+        self.force = (g * obj0.mass * obj1.mass) / (math.dist(obj0.position, obj1.position) ** 2) #Force
+        self.gForce = (self.direction * self.force)  #Vector Created
+        self.direction = (0,0)
+        self.force = 0
