edfcb189f968552f2280af264aa2f121603be997
Alexander M <aardvarkchase@gmail.com>
2025-07-09 22:33:27 -0400

    Made the renderer produce random colors for visual noise (I was tired of looking at only red)

--- Render.py
+++ Render.py
@@ -1,6 +1,6 @@
 import pygame
-
+import random
 
 def renderPlanets(screen, pixelArray, radius):
     for x in pixelArray:
-        pygame.draw.circle(screen, (255,0,0),x.getPosition(), radius)
+        pygame.draw.circle(screen, (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)),x.getPosition(), radius)
