c88ae93604108b70937c9d896c52866d9d75d79e
Alexander M <aardvarkchase@gmail.com>
2025-07-11 14:56:31 -0400

    Slight refactor allowing for offsets to be added in the future, this should translate with "Moving" the camera around

--- Render.py
+++ Render.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 
-def renderPlanets(screen, pixelArray, radius):
+CURRENT_OFFSET_X = 0
+CURRENT_OFFSET_Y = 0
+
+def renderPlanets(screen, pixelArray, radius, CURRENT_OFFSET):
     for x in pixelArray:
-        pygame.draw.circle(screen, x.color, x.getPosition(), radius * (x.mass ** (1/5)))
+        pygame.draw.circle(screen, x.color, pygame.Vector2(x.getPosition())+pygame.Vector2(CURRENT_OFFSET[0], CURRENT_OFFSET[1]), radius * (x.radius))
