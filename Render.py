3008417771efd55cb50ce2471553c304c5800aa5
Alexander M <aardvarkchase@gmail.com>
2025-07-13 14:57:40 -0400

    Improved the capabilities of this class, allowing it to also render the positional histories of planets.

--- Render.py
+++ Render.py
@@ -1,9 +1,20 @@
 import pygame
 import random
 
-CURRENT_OFFSET_X = 0
-CURRENT_OFFSET_Y = 0
+class Render:
+    def __init__(self, name):
+        CURRENT_OFFSET_X = 0
+        CURRENT_OFFSET_Y = 0
 
-def renderPlanets(screen, pixelArray, radius, CURRENT_OFFSET):
-    for x in pixelArray:
-        pygame.draw.circle(screen, x.color, pygame.Vector2(x.getPosition())+pygame.Vector2(CURRENT_OFFSET[0], CURRENT_OFFSET[1]), x.radius)
+
+    def renderPlanets(self, screen, pixelArray, radius, CURRENT_OFFSET):
+        for x in pixelArray:
+            pygame.draw.circle(screen, x.color, pygame.Vector2(x.getPosition())+pygame.Vector2(CURRENT_OFFSET[0], CURRENT_OFFSET[1]), x.radius)
+
+    def draw_history(self, screen, pixelArray, CURRENT_OFFSET):
+        for x in pixelArray:
+            position_history = list(x.getPositionHistory())
+            position_history.reverse()
+            if (len(x.getPositionHistory()) > 2):
+                for i in range(1, len(position_history)):
+                    pygame.draw.line(screen, (255, 255, 255), position_history[i], position_history[2], 1)
