d35dee7fdef617664dc8e122bc266f63ecd2acd1
Slashy Slasher <aardvarkchase@gmail.com>
2025-07-15 15:37:32 -0400

    Added zoom functionality to the Render method, disabled draw_history temporarily.

--- Render.py
+++ Render.py
@@ -7,14 +7,32 @@ class Render:
         CURRENT_OFFSET_Y = 0
 
 
-    def renderPlanets(self, screen, pixelArray, radius, CURRENT_OFFSET):
-        for x in pixelArray:
-            pygame.draw.circle(screen, x.color, pygame.Vector2(x.getPosition())+pygame.Vector2(CURRENT_OFFSET[0], CURRENT_OFFSET[1]), x.radius)
+    #def renderPlanets(self, screen, pixelArray, radius, CURRENT_OFFSET, zoom):
+    #    for x in pixelArray:
+    #        pygame.draw.circle(screen, x.color, pygame.Vector2(x.getPosition())+pygame.Vector2(CURRENT_OFFSET[0], CURRENT_OFFSET[1]), x.radius)
 
-    def draw_history(self, screen, pixelArray, CURRENT_OFFSET):
+    def renderPlanets(self, screen, pixelArray, CURRENT_OFFSET, zoom):
         for x in pixelArray:
-            position_history = list(x.getPositionHistory())
-            position_history.reverse()
-            if (len(x.getPositionHistory()) > 2):
-                for i in range(1, len(position_history)):
-                    pygame.draw.line(screen, (255, 255, 255), position_history[i], position_history[2], 1)
+            world_pos = pygame.Vector2(x.getPosition())
+            screen_pos = (world_pos + pygame.Vector2(CURRENT_OFFSET)) * zoom
+            scaled_radius = max(1, int(x.radius * zoom))
+            pygame.draw.circle(screen, x.color, screen_pos, scaled_radius)
+
+    #def draw_history(self, screen, pixelArray, CURRENT_OFFSET, zoom):
+    #    for x in pixelArray:
+    #        position_history = list(x.getPositionHistory())
+    #        position_history.reverse()
+    #        if (len(x.getPositionHistory()) > 2):
+    #            for i in range(1, len(position_history)):
+    #                pygame.draw.line(screen, (255, 255, 255), position_history[i], position_history[2], 1)
+
+    def scale_world(self, screen, zoom):
+        scaled_world = pygame.transform.smoothscale(
+            screen,
+            (int(screen.get_width() * zoom), int(screen.get_height() * zoom))
+        )
+        screen.fill((0, 0, 0))
+        # Center the scaled world in the screen
+        world_rect = scaled_world.get_rect(center=screen.get_rect().center)
+
+        return screen.blit(scaled_world, world_rect)
